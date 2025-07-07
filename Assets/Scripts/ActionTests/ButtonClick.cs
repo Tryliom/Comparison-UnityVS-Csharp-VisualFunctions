dfec28403708fd571b9a7e57a095b2dc87d5fce5
Tryliom <alexishaldy@gmail.com>
2025-07-07 18:08:31 +0200

    Added action tests

---
+++ Assets/Scripts/ActionTests/ButtonClick.cs
@@ -0,0 +1,15 @@
+﻿using TMPro;
+using UnityEngine;
+
+public class ButtonClick : MonoBehaviour
+{
+    [SerializeField] private TextMeshProUGUI _buttonText;
+    
+    private int _clickCount;
+
+    public void OnClick()
+    {
+        _clickCount++;
+        _buttonText.text = _clickCount.ToString();
+    }
+}
---
+++ Assets/Scripts/ActionTests/ColliderEvent.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+public class OnEnter : MonoBehaviour
+{
+    [SerializeField] private GameObject _objectToToggle;
+    
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            _objectToToggle.SetActive(false);
+        }
+    }
+    
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            _objectToToggle.SetActive(true);
+        }
+    }
+}
